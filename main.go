ea7d00e9278b1cefea6eea0b60dc1d3d0b2e3591
Adrephos <adrephos@gmail.com>
2023-05-24 16:02:42 -0500

    feat: add playing option

--- main.go
+++ main.go
@@ -11,14 +11,26 @@ import (
 func main() {
 	
 	args := os.Args[1:]
+	var outputPath string
 
 	if len(args) >= 3 {
-		mix, err := src.Mix(args[0], args[1])
+		index := 0
+		if args[0] == "-p" {
+			index = 1
+		}
+		fmt.Println("Mxing songs ....")
+		mix, err := src.Mix(args[index], args[index + 1])
 
 		if err != nil {
 			fmt.Println(err)
 		} else {
-			godub.NewExporter(fmt.Sprintf("%s", args[2])).WithDstFormat("mp3").Export(mix)
+			outputPath = fmt.Sprintf("%s", args[index + 2])
+			godub.NewExporter(outputPath).WithDstFormat("mp3").Export(mix)
+			fmt.Println("Done")
+		}
+
+		if index == 1 {
+			src.Play(outputPath, mix.Duration())
 		}
 
 	}	else {
--- src/mix.go
+++ src/mix.go
@@ -1,6 +1,12 @@
 package src
 
 import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"time"
+
 	"github.com/iFaceless/godub"
 )
 
@@ -24,11 +30,58 @@ func Mix(path1 string, path2 string) (*godub.AudioSegment, error) {
 	} else {
 		overlaidSeg, err = segment2.Overlay(segment, &godub.OverlayConfig{LoopToEnd: false})
 	}
-	
 
 	if err != nil {
 		return nil, err
 	}
 
 	return overlaidSeg, err
+}
+
+func Play(path string, duration time.Duration) {
+	// Start ffplay command
+	cmd := exec.Command("mpv", "--volume=40", "--no-video", "--quiet", path)
+
+	// Start the command
+	fmt.Println("Playing", path)
+	err := cmd.Start()
+	if err != nil {
+		fmt.Println("Error starting ffplay command:", err)
+		return
+	}
+
+	durationFormat := fmt.Sprintf("%02d:%02d", int(duration.Minutes()), int(duration.Seconds()-float64(int(duration.Minutes())*60)))
+
+	go func() {
+		// Get the initial time.
+		before := time.Now()
+		for {
+			// Get the current time.
+			now := time.Now()
+
+			// Calculate the time remaining in the song.
+			remainingTime := now.Sub(before)
+			remainingFormat := fmt.Sprintf("%02d:%02d", int(remainingTime.Minutes()), int(remainingTime.Seconds()-float64(int(remainingTime.Minutes())*60)))
+			
+			if int(remainingTime.Seconds()) >= int(duration.Seconds()) {
+				break
+			}
+
+			// Print the time remaining in the song.
+			fmt.Printf("\rTime elapsed: %v / %v", remainingFormat, durationFormat)
+
+			// Sleep for 1 second.
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	// Wait for the command to finish
+	err = cmd.Wait()
+	if err != nil {
+		fmt.Println("Command finished with error:", err)
+	}
+
+	// Wait for user input to exit
+	fmt.Print("\rSong finished, press Enter to exit.")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
 }
